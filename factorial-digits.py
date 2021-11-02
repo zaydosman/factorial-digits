2a313c1215f007f5adb5d58f0db23d69127a36e7
Zayd <zaydosman@gmail.com>
2021-11-02 21:44:33 +0200

    tidied up comments

--- factorial-digits.py
+++ factorial-digits.py
@@ -4,14 +4,14 @@ import numpy as np
 def sumfactorial(inp):
 
     
-    fact=np.math.factorial(inp)                                      #gets factorial using numpy
+    fact=np.math.factorial(inp)                                             #gets factorial using numpy
     
-    sum = 0                                                       #variable for sum of digits
+    sum = 0                                                                 #variable for sum of digits
     
-    while fact:                                                    #loop to sum factorial digits
+    while fact:                                                             #loop to sum factorial digits
         sum, fact = np.add(sum, np.mod(fact,10)), fact//10
-        #sum, fact = sum + fact % 10 , fact//10                              #this is faster, does not use numpy though
-        #sum, fact = np.add(sum, np.mod(fact,10)), np.floor_divide(fact,10)  #"np.floor_divide" not giving correct value, for input of 100
+        #sum, fact = sum + fact % 10 , fact//10                             #this is faster, does not use numpy though
+        #sum, fact = np.add(sum, np.mod(fact,10)), np.floor_divide(fact,10) #"np.floor_divide" not giving correct value, for input of 100
 
     return(sum)
 
@@ -19,7 +19,7 @@ def sumfactorial(inp):
 
 if __name__ == "__main__":    
     
-    try:                                                        #checks for valid input arguments
+    try:                                                                    #checks for valid input arguments
         sys.argv[1].isdigit()                               
         print(sumfactorial(int(sys.argv[1])))
     except ValueError:
